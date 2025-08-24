e8488fa4ab14e547d7b71d514701f789663a3bf8
Marcelo Moro <101741773+marcelomoro1@users.noreply.github.com>
2025-08-24 12:35:43 -0300

    ReceitaViewController.java

--- src/main/java/com/mjwsolucoes/sistemanutricao/controller/ReceitaViewController.java
+++ src/main/java/com/mjwsolucoes/sistemanutricao/controller/ReceitaViewController.java
@@ -2,7 +2,7 @@ package com.mjwsolucoes.sistemanutricao.controller;
 
 import com.mjwsolucoes.sistemanutricao.dto.ReceitaDetalhadaDTO;
 import com.mjwsolucoes.sistemanutricao.service.ReceitaService;
-import org.springframework.stereotype.Controller; // Use @Controller para retornar views, não @RestController
+import org.springframework.stereotype.Controller; 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,16 +18,14 @@ public class ReceitaViewController {
         this.receitaService = receitaService;
     }
 
-    // Este é o método que você precisa adicionar
     @GetMapping("/detalhes/{id}")
     public String exibirDetalhesReceita(@PathVariable Long id, Model model) {
         ReceitaDetalhadaDTO detalhes = receitaService.buscarDetalhesReceita(id);
 
-        // Adiciona os detalhes ao modelo para que o Thymeleaf possa acessá-los
         model.addAttribute("receitaDetalhes", detalhes); 
 
         
-        return "detalhesReceita"; // Certifique-se de que este é o nome do seu arquivo HTML
+        return "detalhesReceita";
     }
 
    
